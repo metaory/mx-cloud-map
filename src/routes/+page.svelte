cc7b7659112d8c4f5e63664cbe1e1499fce3c05f
metaory <metaory@gmail.com>
2022-12-18 14:36:07 +0800

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
+<script>
+  import { Button, FileDropzone, DatePicker } from 'attractions';
+  import * as blobs2 from "blobs/v2";
+  import * as blobs2Animate from "blobs/v2/animate";
+  console.log('blobs2:', blobs2)
+  console.log('blobs2Animate:', blobs2Animate)
+
+  const svgString = blobs2.svg(
+    {
+        seed: Math.random(),
+        extraPoints: 8,
+        randomness: 4,
+        size: 256,
+    },
+    {
+        fill: "white", // 🚨 NOT SANITIZED
+        stroke: "black", // 🚨 NOT SANITIZED
+        strokeWidth: 4,
+    },
+  );
+  console.log('svgString:', svgString) 
+  /* container.innerHTML = svgString; */
+</script>
+
+<Button>click me</Button>
+
 <h1>Welcome to Xorg</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<FileDropzone accept="image/*" max={3} />
+
+<DatePicker format="%m/%d/%Y" closeOnSelection />
+
+<Button filled>button</Button>
+<Button outline>button</Button>
+<Button>button</Button>
+
+<Button disabled filled>button</Button>
+<Button disabled outline>button</Button>
+<Button disabled>button</Button>
+
+<Button danger filled>button</Button>
+<Button danger outline>button</Button>
+
+<style lang="scss">
+  /* because the directory of `theme.scss` is in `includePaths` */
+  @use 'theme.scss';
+
+  $main: red;
+  p {
+    color: theme.$main;
+  }
+  a {
+    color: theme.$foo;
+  }
+</style>
+
+
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,33 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-cloudflare';
+import sveltePreprocess from 'svelte-preprocess';
+import makeAttractionsImporter from 'attractions/importer.js';
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  // preprocess: [sveltePreprocess()],
+  // preprocess: [
+  //   sveltePreprocess({
+  //     scss: {
+  //       importer: makeAttractionsImporter(),
+  //     },
+  //   }),
+  // ],
+  preprocess: [
+    sveltePreprocess({
+      scss: {
+        importer: makeAttractionsImporter({
+          themeFile: './static/css/theme.scss'
+        }),
+        includePaths: ['./static/css'],
+      },
+    }),
+  ],
+
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
